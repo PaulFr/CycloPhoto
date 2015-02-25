a590b803d2be37f362138eed6281139424bec23c
Paul Frinchaboy <pfrinchaboy@gmail.com>
2015-02-26 00:49:39 +0100

    Envoie des photos a "l'achat"

--- src/Projet/PhotoBundle/Controller/PhotosController.php
+++ src/Projet/PhotoBundle/Controller/PhotosController.php
@@ -5,6 +5,7 @@ namespace Projet\PhotoBundle\Controller;
 use Projet\PhotoBundle\Entity\Achat;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 
 class PhotosController extends Controller
@@ -71,7 +72,7 @@ class PhotosController extends Controller
 
             $panier->addPhoto($photo);
             $session->set('panier', $panier);
-            return $this->redirect($this->generateUrl("projet_photo_panier"));
+            return $this->redirect($this->generateUrl("projet_photo_myphotos"));
         }
 
         throw $this->createNotFoundException("Photo non trouvée");
@@ -83,6 +84,50 @@ class PhotosController extends Controller
         $participation = $session->get('participation');
         if ($participation == null) throw $this->createNotFoundException("Participation non existante");
         return $participation;
+    }
+
+    public function panierValideAction()
+    {
+        $participation = $this->canAccess();
+        $session = $this->get('session');
+        $panier = $session->get('panier');
+        $em = $this->getDoctrine()->getManager();
+        /*$em->persist($panier);
+        $em->flush();*/
+        $zip = $this->createZip($panier);
+        $this->sendPhotos($zip, $participation);
+
+        $session->set('panier', new Achat());
+        //unlink($zip);
+
+        return $this->render("ProjetPhotoBundle:Photos:valide.html.twig");
+    }
+
+    public function createZip($panier)
+    {
+        $zip = new \ZipArchive();
+        $file = __DIR__ . '/../../../../depot_photos/' . time() . '.zip';
+        $res = $zip->open($file, \ZipArchive::CREATE);
+
+        if ($res !== true) throw new Exception();
+
+        foreach ($panier->getPhotos() as $photo) {
+            $zip->addFile(__DIR__ . '/../../../../photos/' . $photo->getId() . '.jpg', $photo->getId() . '.jpg');
+        }
+        $zip->close();
+        return $file;
+    }
+
+    public function sendPhotos($zip, $participation)
+    {
+        $mailer = $this->get('mailer');
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Vos photos')
+            ->setFrom('cyclophotographie@gmail.com')
+            ->setTo($participation->getPersonne()->getEmail())
+            ->setBody($this->renderView('ProjetPhotoBundle:Photos:achat-email.txt.twig', array('participation' => $participation)))
+            ->attach(\Swift_Attachment::fromPath($zip));
+        $mailer->send($message);
     }
 
     public function deletePanierAction($id)
